a279a9a516db656a0a6e5f7753fca63b20d9a736
Chelsea Farley <chelseafarley91@gmail.com>
2020-05-01 16:19:39 +1200

    Fixing demo

--- ReactNativeAdmobAdsDemo/App.js
+++ ReactNativeAdmobAdsDemo/App.js
@@ -1,40 +1,42 @@
-import React from 'react';
-import { Text, Button } from 'react-native';
-import { AdView } from 'react-native-admob-ads';
+import React, { Component } from 'react';
+import { Text, Button, View } from 'react-native';
+import AdView from 'react-native-admob-ads';
 
-export default function App() {
+export default class App extends Component {
   getTopContent() {
-    return <Text>Header</Text>
+    return <Text style={{marginVertical: 32}}>Header</Text>
   }
 
   getBottomContent() {
-    return <Text>Footer</Text>;
+    return <Text style={{marginVertical: 16}}>Footer</Text>;
   }
 
   showInterstital() {
     this.refs.adView.displayAd();
   }
 
-  return (
+  render() {
     var interstitialAdId = {
-      ios: "",
-      android: ""
+      ios: "YOUR IOS INTERSTITIAL AD ID",
+      android: "YOUR ANDROID INTERSTITIAL AD ID"
     };
 
     var bannerAdId = {
-      ios: "",
-      android: ""
+      ios: "YOUR IOS BANNER AD ID",
+      android: "YOUR ANDROID BANNER AD ID"
     };
 
-    <AdView ref="adView"
-      interstitialAdId={interstitialAdId}
-      bannerAdId={bannerAdId}
-      topContent={this.getTopContent()}
-      bottomContent={this.getBottomContent()}>
-      <Button
-        title="Show Interstitial Ad"
-        color="#3CBBB1"
-        onPress={this.showInterstital.bind(this)} />
-    </Adview>
-  );
+    return (
+      <AdView ref="adView"
+        interstitialAdId={interstitialAdId}
+        bannerAdId={bannerAdId}
+        topContent={this.getTopContent()}
+        bottomContent={this.getBottomContent()}>
+        <Button
+          title="Show Interstitial Ad"
+          color="#3CBBB1"
+          onPress={this.showInterstital.bind(this)} />
+      </AdView>
+    );
+  }
 }
